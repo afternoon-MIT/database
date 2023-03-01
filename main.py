685f6c08f278cdbf794c2b08a6300b9923bb6c78
eMobilis <v2785367@gmail.com>
2023-03-01 15:22:34 +0300

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,88 @@
 from peewee import *
+from os import path
+
+
+connection = path.dirname(path.realpath(__file__))
+db = SqliteDatabase(path.join(connection, "Victor.db"))
+
+#creating our first table
+
+
+class Student(Model):
+    stud_name = CharField()
+    stud_email = CharField()
+    stud_password = CharField()
+
+    class Meta:
+        database = db
+
+Student.create_table(fail_silently=True)
+
+
+
+
+
+from peewee import *
+from os import path
+
+
+connection = path.dirname(path.realpath(__file__))
+db = SqliteDatabase(path.join(connection, "Victor.db"))
+
+#creating our first table
+
+
+class Teacher(Model):
+    Teacher_name = CharField()
+    Teacher_email = CharField()
+    Teacher_password = CharField()
+
+    class Meta:
+        database = db
+
+Student.create_table(fail_silently=True)
+
+
+
+from peewee import *
+from os import path
+
+
+connection = path.dirname(path.realpath(__file__))
+db = SqliteDatabase(path.join(connection, "Victor.db"))
+
+#creating our first table
+
+
+class Product(Model):
+    Product_price = CharField()
+    Product_quantity = CharField()
+    Product_description = CharField()
+    product_color = CharField()
+
+    class Meta:
+        database = db
+
+Student.create_table(fail_silently=True)
+
+
+from peewee import *
+from os import path
+
+
+connection = path.dirname(path.realpath(__file__))
+db = SqliteDatabase(path.join(connection, "Victor.db"))
+
+#creating our first table
+
+
+class User(Model):
+    User_name = CharField()
+    User_phone = CharField()
+    User_email = CharField()
+    User_password = CharField()
+
+    class Meta:
+        database = db
+
+Student.create_table(fail_silently=True)
---
+++ register_students.py
@@ -0,0 +1,13 @@
+from main import Student
+
+
+try:
+    student_name = input("Enter name \n")
+    student_email = input("Enter email \n")
+    student_password = input("Enter password \n")
+
+
+    Student.create( stud_name=student_name, stud_email=student_email, stud_password=student_password)
+    print("Student Created Successfully")
+except:
+    print("Fail to create student")
